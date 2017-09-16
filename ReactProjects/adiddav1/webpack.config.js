a002af58e6e22d1097727457d57449d2c4973b4f
raghavsurya <raghav2787@gmail.com>
2017-09-16 15:37:58 +0100

    first commit

---
+++ ReactProjects/adiddav1/webpack.config.js
@@ -0,0 +1,18 @@
+
+const webpack = require('webpack'); //to access built-in plugins
+const path = require('path');
+
+const config = {
+  entry: ['babel-polyfill','./lib/components/index.js'],
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      { test: /\.js$/, exclude: /node_modules/, use: 'babel-loader' }
+    ]
+  }
+};
+
+module.exports = config;
